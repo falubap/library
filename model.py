4248230abd67a0f0bc842e778c290fdd5b474131
falubap <pedrofaluba@gmail.com>
2025-04-04 19:57:23 -0300

    Extracting methode get_charge from Client.statement

--- model.py
+++ model.py
@@ -22,6 +22,21 @@ class Client:
     def add_rental(self, rental: Rental):
         self.rentals.append(rental)
 
+    def get_charge(self, rental: Rental) -> float:
+        amount = 0
+        if rental.book.price_code == Book.REGULAR:
+            amount += 2
+            if rental.days_rented > 2:
+                amount += (rental.days_rented - 2) * 1.5
+        elif rental.book.price_code == Book.NEW_RELEASE:
+            amount += rental.days_rented * 3
+        elif rental.book.price_code == Book.CHILDREN:
+            amount += 1.5
+            if rental.days_rented > 3:
+                amount += (rental.days_rented - 3) * 1.5
+        return amount
+
+
     def statement(self) -> str:
 
         total_amount = 0
@@ -32,17 +47,8 @@ class Client:
             amount = 0
             
             # determine amounts for each line
-            if rental.book.price_code == Book.REGULAR:
-                amount += 2
-                if rental.days_rented > 2:
-                    amount += (rental.days_rented - 2) * 1.5
-            elif rental.book.price_code == Book.NEW_RELEASE:
-                amount += rental.days_rented * 3
-            elif rental.book.price_code == Book.CHILDREN:
-                amount += 1.5
-                if rental.days_rented > 3:
-                    amount += (rental.days_rented - 3) * 1.5
-
+            amount = self.get_charge(rental)
+            
             # add frequent renter points
             frequent_renter_points += 1
             if rental.book.price_code == Book.NEW_RELEASE and rental.days_rented > 1:
